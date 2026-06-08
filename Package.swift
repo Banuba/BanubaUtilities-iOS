3b7616151c996a7479e9e01dc2a83e6e78150e67
CI <ci@banuba.com>
2026-06-08 21:34:37 +0300

    automatic framework update

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "BanubaUtilities",
-      path: "BanubaUtilities.xcframework"
+      url: "https://nexus.banuba.net/repository/ios-frameworks/ios/frameworks/BanubaUtilities/1.53.0-beta.0/BanubaUtilities-1.53.0-beta.0.xcframework.zip",
+      checksum: "dabd55e54e0e8ef7f3b142f89b49e5d72bcc380050f332dc4c0ce1ec2204ef10"
     )
   ]
 )
